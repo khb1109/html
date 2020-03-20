7f70aa6987caad2b070d67abc63b6cebedb040a5
hongbin23 <khbasd@gmail.com>
2020-03-20 17:13:28 +0900

    feat: 이메일 구현 및 텍스트 수정

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,14 @@
 $(document).ready(function(){
     $("input:radio[name=radio]").click(function(){
         pageNumber = window.location.href.split("#")[1];
-        if(pageNumber == null) {
-            location.href="/index.html#2";
-        }
         if($(this).attr('id')==='o'){
+            if(pageNumber == null) {
+                location.href="/index.html#2";
+            }
             location.href="/index.html#" + (Number(pageNumber)+1);
         }else{
             alert("틀렸습니다!");
         }
     });
 });
+

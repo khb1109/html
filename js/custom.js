88c697510474697b16cff2d5d5d618950e19ba43
hongbin23 <khbasd@gmail.com>
2020-03-21 13:59:50 +0900

    feat: 모달 추가, 메인 페이지 추가

--- js/custom.js
+++ js/custom.js
@@ -1,16 +1,28 @@
 $(document).ready(function () {
     $("input:radio[name=radio]").click(function () {
-        var pageNumber = window.location.href.split("#")[1];
         if ($(this).attr('id') === 'o') {
-            if (pageNumber === null || pageNumber == undefined) {
-                location.href = "/index.html#2";
-            }
-            else {
-                location.href = "/index.html#" + (Number(pageNumber) + 1);
-            }
+            $('#myModal').modal(focus);
+
+            // if (pageNumber === null || pageNumber == undefined) {
+            //     location.href = "/index.html#2";
+            // }
+            // else {
+            //     location.href = "/index.html#" + (Number(pageNumber) + 1);
+            // }
         } else {
             alert("틀렸습니다!");
         }
     });
 });
 
+
+$("#nextButton").click(function(){
+    $('#myModal').modal('hide');
+    var pageNumber = window.location.href.split("#")[1];
+    if (pageNumber === null || pageNumber == undefined) {
+        location.href = "/index.html#2";
+    }
+    else {
+        location.href = "/index.html#" + (Number(pageNumber) + 1);
+    }
+});
